42937ece900853d174eeb292d6e68413da8a3a23
Gavin Daly <gavin@gav.local>
2019-06-25 13:54:23 -0400

    Load with Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React, { lazy } from "react";
+import React, { lazy, Suspense } from "react";
 
 import { useAuth } from "./hooks/authContext";
 
 import "./Global.css";
 
-// const AuthenticatedApp = lazy(() => import("./AuthenticatedApp"));
-// const LogIn = lazy(() => import("./LogIn"));
-
-import AuthenticatedApp from "./AuthenticatedApp";
-import LogIn from "./LogIn";
+const AuthenticatedApp = lazy(() => import("./AuthenticatedApp"));
+const LogIn = lazy(() => import("./LogIn"));
 
 function App() {
-  // const { currentUser } = useUser();
   const { jwt } = useAuth();
-  return <>{jwt ? <AuthenticatedApp /> : <LogIn />}</>;
+  return (
+    <Suspense fallback="Loading...">
+      {jwt ? <AuthenticatedApp /> : <LogIn />}
+    </Suspense>
+  );
 }
 
 export default App;
