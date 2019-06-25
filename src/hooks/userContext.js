cbd6f8f160011922090989c763b121cf58a7f470
Gavin Daly <gavin@gav.local>
2019-06-25 15:01:31 -0400

    get details for current user

--- src/hooks/userContext.js
+++ src/hooks/userContext.js
@@ -1,9 +1,16 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import jwtDecode from "jwt-decode";
 
+import client from "../utils/apiClient";
+
 import { useAuth } from "./authContext";
 
 const UserContext = createContext();
+
+async function updateUserDetails(token_values, updateUser) {
+  // const details = client(); Get more info in the future
+  updateUser(...token_values);
+}
 
 const UserProvider = props => {
   const [currentUser, setCurrentUser] = useState(null);
@@ -15,9 +22,19 @@ const UserProvider = props => {
       return;
     }
     setCurrentUser(jwtDecode(jwt));
+    updateUserDetails(jwtDecode(jwt), setCurrentUser);
   }, [jwt]);
 
-  return <UserContext.Provider value={{ currentUser, jwt }} {...props} />;
+  function updateDetails() {
+    updateUserDetails(jwtDecode(jwt), setCurrentUser);
+  }
+
+  return (
+    <UserContext.Provider
+      value={{ currentUser, jwt, updateDetails }}
+      {...props}
+    />
+  );
 };
 
 const useUser = () => useContext(UserContext);
