a8f7f1bf3eebee13dd630792a59bf2554a78066f
Gavin Daly <gavin@gav.local>
2019-07-02 21:51:15 -0400

    shows all the entry times

--- src/components/Timesheet/Entry.jsx
+++ src/components/Timesheet/Entry.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { format } from "date-fns";
+import { useTimeSheet } from "../../hooks/timeSheetContext";
 
 export const Entries = ({ entries }) => (
   <div className="entires">
@@ -20,14 +21,16 @@ export const DayEntry = ({ dayEntry, day }) => (
   </div>
 );
 
-const CorrectingEntry = ({ setIsCorrecting, start, end }) => {
+const CorrectingEntry = ({ setIsCorrecting, start, end, id }) => {
   const [formState, setFormState] = useState("invalid");
   const [value, setValue] = useState({ start, end, reason: "" });
+
+  const { correctEntry } = useTimeSheet();
 
   function submit(event) {
     event.preventDefault();
     setFormState("submitting");
-    // submitCorrectingEntry({ startTime, endTime, reason });
+    correctEntry({ ...value, id });
   }
 
   useEffect(() => {
@@ -88,7 +91,7 @@ const EditableEntry = ({ entry }) => {
   const displayStart = format(entry.start_time, "h:mm aa");
   const displayEnd = format(entry.end_time, "h:mm aa");
   const [isCorrecting, setIsCorrecting] = useState(false);
-  console.log("Entry", entry);
+
   return (
     <div className="entry time state_editable">
       <div className="timeGroup">
@@ -105,6 +108,7 @@ const EditableEntry = ({ entry }) => {
           start={start}
           end={end}
           setIsCorrecting={setIsCorrecting}
+          id={entry.id}
         />
       )}
       <div className="state editable" onClick={() => setIsCorrecting(true)}>
@@ -115,27 +119,34 @@ const EditableEntry = ({ entry }) => {
 };
 
 const HoursEntry = ({ entry }) => {
+  const displayStart = format(entry.start_time, "h:mm aa");
+  const displayEnd = format(entry.end_time, "h:mm aa");
+
+  const correction = entry.correction;
+  const correctionStart = format(correction.start_time, "h:mm aa");
+  const correctionEnd = format(correction.end_time, "h:mm aa");
+  const originalStart = format(correction.original_start_time, "h:mm aa");
+  const originalEnd = format(correction.original_end_time, "h:mm aa");
   switch (entry.state) {
     case "pending":
-      const correction = {};
       return (
         <div className="entry time state_pending">
           <div className="correction">
             <div className="correction_reason">{correction.reason}</div>
             <div className="time_group">
               <div className="original_start">
-                Original Start: <span>9:00am</span>
+                Original Start: <span>{displayStart}</span>
               </div>
               <div className="original_end">
-                Original End: <span>5:00pm</span>
+                Original End: <span>{displayEnd}</span>
               </div>
             </div>
             <div className="time_group">
               <div className="requested_start">
-                Requested Start: <span>8:00am</span>
+                Requested Start: <span>{correctionStart}</span>
               </div>
               <div className="requested_end">
-                Requested End: <span>5:00pm</span>
+                Requested End: <span>{correctionEnd}</span>
               </div>
             </div>
           </div>
@@ -147,10 +158,10 @@ const HoursEntry = ({ entry }) => {
         <div className="entry time state_finalized">
           <div className="time_group">
             <div className="time_start">
-              Start: <span>8:00am</span>
+              Start: <span>{displayStart}</span>
             </div>
             <div className="time_end">
-              End: <span>5:00pm</span>
+              End: <span>{displayEnd}</span>
             </div>
           </div>
 
@@ -163,19 +174,19 @@ const HoursEntry = ({ entry }) => {
       return (
         <div className="entry time state_accepted">
           <div className="time_start">
-            Start: <span>8:00am</span>
+            Start: <span>{displayStart}</span>
           </div>
           <div className="time_end">
-            End: <span>5:00pm</span>
+            End: <span>{displayEnd}</span>
           </div>
 
           <div className="correction">
             <div className="correction_reason">{correction.reason}</div>
             <div className="original_start">
-              Original Start: <span>9:00am</span>
+              Original Start: <span>{originalStart}</span>
             </div>
             <div className="original_end">
-              Original End: <span>5:00pm</span>
+              Original End: <span>{originalEnd}</span>
             </div>
             <div className="correction_explanation">{correction.response}</div>
           </div>
@@ -186,27 +197,29 @@ const HoursEntry = ({ entry }) => {
       return (
         <div className="entry time state_rejected">
           <div className="time_start">
-            Start: <span>8:00am</span>
+            Start: <span>{displayStart}</span>
           </div>
           <div className="time_end">
-            End: <span>5:00pm</span>
+            End: <span>{displayEnd}</span>
           </div>
 
           <div className="correction">
             <div className="correction_reason">{correction.reason}</div>
             <div className="requested_start">
-              Requested Start: <span>{correction.startTime}</span>
+              Requested Start: <span>{correctionStart}</span>
             </div>
             <div className="requested_end">
-              Requested End: <span>{correction.endTime}</span>
+              Requested End: <span>{correctionEnd}</span>
             </div>
-            <div className="correction_explanation">OK!</div>
+            <div className="correction_explanation">
+              {correction.explanation}
+            </div>
           </div>
           <div className="state">Rejected</div>
         </div>
       );
     default:
-      new Error("incorrect hour entry state");
+      new Error("incorrect entry state");
   }
 };
 
@@ -230,14 +243,17 @@ const VacationEntry = ({ entry }) => {
   );
 };
 
-const AdjustmentEntry = ({ entry }) => (
-  <div className="entry adjustment">
-    <div className="duration">
-      Hours Added: <span>{entry.duration}h</span>
+const AdjustmentEntry = ({ entry }) => {
+  const classNames = `entry ${entry.category}`;
+  return (
+    <div className={classNames}>
+      <div className="duration">
+        Hours Added: <span>{entry.duration}h</span>
+      </div>
+      <div className="reason">{entry.reason}</div>
     </div>
-    <div className="reason">{entry.reason}</div>
-  </div>
-);
+  );
+};
 
 export const Entry = ({ entry }) => {
   switch (entry.type) {
@@ -248,6 +264,6 @@ export const Entry = ({ entry }) => {
     case "adjustment":
       return <AdjustmentEntry entry={entry} />;
     default:
-      new Error(`incorrect entry type: ${entry}`);
+      new Error(`incorrect entry type: ${JSON.stringify(entry)}`);
   }
 };
