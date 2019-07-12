31365c92df2f6fe5ef2948854367ea213c313807
Gavin Daly <gavin@gav.local>
2019-07-12 02:01:47 -0400

    get users

--- src/screens/Users.jsx
+++ src/screens/Users.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import client from "../utils/apiClient";
+import { closestIndexTo } from "date-fns";
 
 export default function Users() {
-  return <>Users</>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const data = await client("GET", "https://");
+      setUsers(data);
+    })();
+  }, []);
+
+  return <>{users.map(user => JSON.parse(user))}</>;
 }
